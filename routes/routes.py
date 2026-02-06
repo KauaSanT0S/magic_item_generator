7fb8b32fdf8789d52bc2455fe4ed9152c51c14ca
Kauã Santos de Barros <kauasantosdebarros@gmail.com>
2026-02-06 18:41:40 -0300

    Adding remaining archives

--- routes/routes.py
+++ routes/routes.py
@@ -1,15 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for
 
 routes = Blueprint("routes", __name__)
 
 @routes.route("/")
 def homepage():
-    return "Essa é a homepage"
+    return render_template("index.html")
 
 @routes.route("/profile")
 def profile():
     return "Esse é seu perfil"
 
-@routes.route("/blog")
-def blog():
-    return "Esse é o blog"
+@routes.route("/generate-items", methods=["GET", "POST"])
+def generate_items():
+    if request.method == "POST":
+        data = request.get_data("/generate_items")
+        print(data)
+        return redirect(url_for("routes.homepage"))
+
+    return render_template("generate-items.html")
